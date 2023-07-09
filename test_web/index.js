b9ad70e2473cfa1b9ff604a1584409ba62c5e190
Sujan Rijal <srijal@castera.io>
2023-07-08 23:48:12 -0400

    random

---
+++ test_web/index.js
@@ -0,0 +1,16 @@
+console.log("Helo world");
+// values and variable
+// A variable is a container for storing a data
+// A variable behaves as if it was the value  that it contains
+// Decleration (Var, let, const)
+// Assignment ( =  assignment operator)
+
+
+var myName = 'Sujan Riajl';
+let myAge = 24;
+const id = 199881;
+
+console.log(myName, myAge, id);
+
+// data types
+console.log(typeof(myAge));
